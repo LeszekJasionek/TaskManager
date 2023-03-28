bc1076569010cd4b6cb74ae64e15d7071f518f3a
LeszekJasionek <leszek.jasionek.bee@outlook.com>
2023-03-28 12:34:57 +0200

    Adding Create, Update , Details

---
+++ Models/TaskModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace TaskManager.Models
+{
+    public class TaskModel
+    {
+        public int TaskId { get; set; }
+        [DisplayName("Name")]
+        public string Name { get; set; }
+        [DisplayName("Description")]
+        public string Description { get; set; }
+        public bool Done { get; set; }
+    }
+}
